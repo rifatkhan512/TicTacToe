430fe3127c94355d2e51255469fb7241ff1865bc
Rifat Khan <60955461+rifatkhan512@users.noreply.github.com>
2020-02-21 18:05:11 +0600

    Update game.py

--- games_file/game.py
+++ games_file/game.py
@@ -1,14 +1,48 @@
 import tkinter as tk
+import turtle as tl
+import design
+import level
 import Chok
 
-root= tk.Tk()
-root.title("Rifat's tic tac toe")
-root.resizable(0,0)
+class Game():
+    
+    def __init__(self):
+        self.root=tk.Tk()
+        self.root.geometry("1920x1080")
+        self.root.title("Rifat's Tic Tac Toe")
+        self.root["bg"]="black"
+        self.root.resizable(0,0)
+        self.root.iconbitmap("tictactoe.ico")
+        self.root.state("zoomed")
+        self.f=tk.Frame(self.root)
+        self.f.pack()
+        self.c=tk.Canvas(self.f,height=1080,width=1920,bg="Blue")
+        
+        self.sc=tl.TurtleScreen(self.c)
+        
+        self.c.pack()
+        self.tt=tl.RawTurtle(self.sc)
+        self.sc.bgcolor("Black")
+        
+        
+    def make_intro(self):
+        design.intro(self.tt)
+        self.c.destroy()
+        self.f.destroy()
+    def level_menu(self):
+        self.ls=level.Levelscreen(self.root)
+        
+    def make_board(self):
+        self.bf=tk.Frame(self.root,bg="yellow")
+        self.bf.pack(anchor="n")
+        b=Chok.Board("X",self.bf)
+        
+        
+        
+a=Game()
+a.make_intro()
+a.level_menu()
+        
+        
+    
 
-current_player="O"
-
-a=Chok.Board(current_player,root)
-
-a.newboard()
-
-root.mainloop()
